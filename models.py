62e5637f0511914fdd014bff2dd93bde9972d195
23f1000687 <147296591+23f1000687@users.noreply.github.com>
2025-01-15 22:55:45 +0530

    Update profile page models.py

--- models.py
+++ models.py
@@ -1,58 +1,136 @@
-from app import app
-from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime
+from flask import Flask, render_template, current_app as app, request, redirect, url_for, flash, session
+from models import db, User
+from werkzeug.security import generate_password_hash, check_password_hash
+from functools import wraps
 
-db = SQLAlchemy(app)
-
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key = True)
-    username = db.Column(db.String(50), unique = True, nullable = False)
-    password_hash = db.Column(db.String(256),  nullable = False)
-    full_name = db.Column(db.String(100), nullable = True)
-    qualification = db.Column(db.String(255), nullable = True)
-    dob = db.Column(db.String(20), nullable = True)
-    is_admin = db.Column(db.Boolean, nullable = False, default = False)
-
-class Subject(db.Model):
-    id = db.Column(db.Integer, primary_key = True)
-    name = db.Column(db.String, nullable = False, unique = True)
-    description = db.Column(db.String(150))
-    # chapters = db.relationship('Chapter', back)
-
-class Chapter(db.Model):
-    id = db.Column(db.Integer, primary_key = True)
-    name = db.Column(db.String(255), nullable = False)
-    description = db.Column(db.String(100))
-    subject_id = db.Column(db.Integer, db.ForeignKey('subject.id'), nullable = False)
-
-class Quiz(db.Model):
-    id = db.Column(db.Integer, primary_key = True)
-    name = db.Column(db.String(255), nullable = False)
-    no_of_chapter = db.Column(db.Integer, nullable = False, foreign_keys = True)
-    no_of_question = db.Column(db.Integer, nullable = False)
-    marks = db.Column(db.Integer, nullable = False)
-    timestamp = db.Column(db.Integer, default=lambda: int(datetime.utcnow().timestamp()))
+admin = User.query.filter_by(is_admin=True).first()
+if not admin:
+    passhash = generate_password_hash('admin')
+    admin = User(username='admin', password_hash=passhash, is_admin=True)
+    db.session.add(admin)
+    db.session.commit()
 
 
-class Question(db.Model):
-    id = db.Column(db.Integer, primary_key = True)
-    # name = db.Column(db.String(30), nullable = False)
-    description = db.Column(db.String(100), nullable = True)
-    option1 = db.Column(db.String(50), nullable = False)
-    option2 = db.Column(db.String(50), nullable = False)
-    option3 = db.Column(db.String(50), nullable = False)
-    option4 = db.Column(db.String(50), nullable = False)
-    correct_option = db.Column(db.String(50), nullable = False)
-    quiz_id = db.Column(db.Integer, db.ForeignKey('subject.id'), nullable = False)
 
-class Score(db.Model):  
-    id = db.Column(db.Integer, primary_key = True)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable = False)
-    quiz_id = db.Column(db.Integer, db.ForeignKey('quiz.id'), nullable = False)
-    no_of_attempt = db.Column(db.Integer, nullable = False)
-    no_of_correct_question = db.Column(db.Integer, nullable = False)
-    timestamp = db.Column(db.Integer, default=lambda: int(datetime.utcnow().timestamp()))
 
-with app.app_context():
-    db.create_all()
-    # db.session.commit()
+@app.route("/login")
+def login():
+    return render_template('login.html')
+
+@app.route("/login", methods=['POST'])
+def login_post():
+    username = request.form.get('username')
+    password = request.form.get('password')
+
+    if not username or not password:
+        flash("Please fill all fields")
+        return redirect(url_for('login'))
+
+    user = User.query.filter_by(username=username).first()
+
+    if not user:
+        flash("Username does not exist")
+        return redirect(url_for('login'))
+
+    if not check_password_hash(user.password_hash, password):
+        flash("Invalid password")
+        return redirect(url_for('login'))
+    # print(url_for('index'))
+    # return redirect(url_for('home'))
+
+    session['user_id'] = user.id
+    flash("Login successful")
+    return redirect(url_for('home'))
+
+@app.route("/register")
+def register():
+    return render_template('register.html')
+
+@app.route("/register", methods=['POST'])
+def register_post():
+    username = request.form.get('username')
+    password = request.form.get('password')
+    full_name = request.form.get('full_name')
+    qualification = request.form.get('qualification')
+    dob = request.form.get('date')
+    
+    if not username or not password or not qualification:
+        flash('Please fill all fields')
+        return redirect(url_for('register'))
+
+    user = User.query.filter_by(username=username).first()
+
+    if user:
+        flash('Username already exists')
+        return redirect(url_for('register'))
+    
+    passhash = generate_password_hash(password)
+
+    new_user = User(username=username, password_hash=passhash, full_name=full_name, qualification=qualification, dob=dob)
+    db.session.add(new_user)
+    db.session.commit()
+    return redirect(url_for('login'))
+
+def auth_requried(func):
+    @wraps(func)
+    def inner(*args, **kwargs):
+        if 'user_id' in session:
+            return func(*args, **kwargs)
+        else:
+            flash('You need to be logged in to access this page')
+            return redirect(url_for('login'))
+    return inner
+
+@app.route("/")
+@auth_requried
+def home():
+    return render_template('index.html')
+
+@app.route("/profile")
+@auth_requried
+def profile():
+    user = User.query.get(session['user_id'])
+    return render_template('profile.html', user=user)
+
+@app.route("/profile", methods=["POST"])
+@auth_requried
+def profile_post():
+    username = request.form.get('username')
+    cpassword = request.form.get('cpassword')
+    password = request.form.get('password')
+    full_name = request.form.get('full_name')
+    dob = request.form.get('date')
+    qualification = request.form.get('qualification')
+
+    if not username or not cpassword or not password:
+        flash('Please fill all fields')
+        return redirect(url_for('profile'))
+
+    user = User.query.get(session['user_id'])
+    if not check_password_hash(user.password_hash, cpassword):
+        flash("Incorrect password !")
+        return redirect(url_for('profile'))
+
+    if username != user.username:
+        new_username = User.query.filter_by(username=username).first()
+        if new_username:
+            flash("Username alreay exits")
+            return redirect(url_for('profile'))
+
+    new_passhash = generate_password_hash(password)
+
+    user.username = username
+    user.password_hash = new_passhash
+    user.full_name = full_name
+    user.date = dob
+    user.qualification = qualification
+    db.session.commit()
+    flash("Profile Updated")
+    return redirect(url_for('profile'))
+    
+
+@app.route("/logout")
+@auth_requried
+def logout():
+    session.pop('user_id')
+    return redirect(url_for('login'))
