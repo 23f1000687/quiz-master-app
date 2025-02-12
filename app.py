3313a6dd12b018c7ac410024193966904c6b6b59
23f1000687 <23f1000687@ds.study.iitm.ac.in>
2025-02-12 15:04:05 +0530

    Added summary charts for quiz statistics and user performance

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
-# from werkzeug.security import generate_password_hash, check_password_hash
 from models import db, User, Subject, Chapter
 
 
@@ -10,17 +9,13 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = 'BK-3QBD15JF8ZuaG5F6HAqMNEHIcHaqQ2Mdz5nvd6kZA4TigbRfGv99-unD7Smo8fOU'
 
-# import models
-# from models import db, User
-
-# models.db.init_app(app)
 db.init_app(app)
 app.app_context().push()
 
 with app.app_context():
     db.create_all()
 
-import routes
+from controllers.routes import *
 
 
 if __name__ == "__main__":
